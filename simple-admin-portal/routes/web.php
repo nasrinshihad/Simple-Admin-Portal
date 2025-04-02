f2efa313586389cd2053d7c70067db392562d29b
nasrin-fathima01 <nasrin@webaune.com>
2025-04-02 09:00:44 +0530

    Route update

--- simple-admin-portal/routes/web.php
+++ simple-admin-portal/routes/web.php
@@ -17,13 +17,10 @@ Route::middleware('auth','admin')->group(function(){
     Route::get('admin/dashboard',[AdminController::class,'index'])->name('admin.dashboard');
     Route::get('admin/create/{type}', [AdminController::class, 'create'])->name('admin.create');
     Route::post('admin/store/{type}', [AdminController::class, 'store'])->name('admin.store');
-    Route::get('/admin/customers', [AdminController::class, 'showList'])->name('admin.customers');
-    Route::get('/admin/invoices', [AdminController::class, 'showList'])->name('admin.invoices');
     Route::get('/admin/data/{type}', [AdminController::class, 'dataIndex'])->name('admin.data.index');
     Route::get('/admin/{type}/{id}/edit', [AdminController::class, 'edit'])->name('admin.edit');
     Route::put('/admin/{type}/{id}/update', [AdminController::class, 'update'])->name('admin.update');
     Route::delete('/admin/{type}/{id}', [AdminController::class, 'destroy'])->name('admin.destroy');
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     
 });
 
